2485d58ca8bcff6a82e2fbfdf9330e4295e3f1fd
UmerMo <umermo23@gmail.com>
2020-01-23 16:02:34 +0000

    Pass props into constructo

--- src/Quiz.js
+++ src/Quiz.js
@@ -3,10 +3,11 @@ import React, { Component } from 'react';
 let quizData = require('./quiz_data.json');
 
 class Quiz extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+
     this.state = {
-      quizPosition: 1
+      quiz_position: 1
     };
   }
 
